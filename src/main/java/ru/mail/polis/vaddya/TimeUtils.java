dff7c4d81365479dbae730a125a43d07ec8403fb
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2019-05-03 21:54:25 +0300

    make currentTimeNanos synchronized to preserve invariant

--- src/main/java/ru/mail/polis/vaddya/TimeUtils.java
+++ src/main/java/ru/mail/polis/vaddya/TimeUtils.java
@@ -1,11 +1,8 @@
 package ru.mail.polis.vaddya;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 public final class TimeUtils {
-    private static final AtomicInteger COUNTER = new AtomicInteger(0);
-    private static final AtomicLong LAST_MILLIS = new AtomicLong(0);
+    private static volatile int COUNTER = 0;
+    private static volatile long LAST_MILLIS = 0L;
 
     private TimeUtils() {
     }
@@ -13,12 +10,12 @@ public final class TimeUtils {
     /**
      * Emulate current time in nanoseconds using System.currentTimeMillis and atomic counter
      */
-    public static long currentTimeNanos() {
-        final var millis = System.currentTimeMillis(); // 100
-        if (LAST_MILLIS.get() != millis) {
-            LAST_MILLIS.set(millis);
-            COUNTER.set(0);
+    public static synchronized long currentTimeNanos() {
+        final var millis = System.currentTimeMillis();
+        if (LAST_MILLIS != millis) {
+            LAST_MILLIS = millis;
+            COUNTER = 0;
         }
-        return LAST_MILLIS.get() * 1_000_000 + COUNTER.getAndIncrement();
+        return LAST_MILLIS * 1_000_000 + COUNTER++;
     }
 }
